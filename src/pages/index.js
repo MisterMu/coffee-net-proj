7a0685077da6de3191fdad72a56a35eadf89ae6c
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-04-07 17:42:21 +0700

    create new shop-page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export * from './login-page/login-page.js';
 export * from './main-page/main-page.js';
 export * from './cart-page/cart-page.js';
+export * from './shop-page/shop-page.js';
--- src/pages/shop-page/shop-page.js
+++ src/pages/shop-page/shop-page.js
@@ -1,46 +1,57 @@
 import React from 'react';
 import { Icon } from 'antd';
 import { Link } from 'react-router-dom';
-import { MainMenu } from '../../components/layouts';
-import '../css/animate.css';
-// import '../css/bootstrap.min.css';
-// import '../css/font.min.css';
-// import '../css/main.css';
-// import '../css/prettyPhoto.css';
-import '../css/price-range.css';
-import '../css/responsive.css';
-import { ItemBox } from '../../components/layouts/item-box/item-box';
+import Axios from 'axios';
+
+import { MainMenu, CategorySideMenu, OtherSideMenu, ItemGroup } from '../../components';
+import './shop-page.scss';
+
 
 export class ShopPage extends React.Component {
-	constructor (props) {
-		super(props);
-		this.state = {
-			item: {}	
-		}
-	}
-
-	componentDidMount () {
-		let tmp = {
-			id:'001',
-			name: 'Sweet Potato',
-			price: '999 Bath',
-		}
-		this.setState({ item: tmp });
-	}
-
-    render () {
-        return (
-            <div>
-            <MainMenu>
-                <Link to="/account"><Icon type="user" /> Account </Link>
-                <Link to="/wishlist"><Icon type="star-o" /> Wishlist </Link>
-                <Link to="/checkout"><Icon type="check-circle-o" /> Checkout </Link>
-                <Link to="/cart"><Icon type="shopping-cart" /> Cart </Link>
-                <Link to="/login"><Icon type="login" /> Login </Link>
-            </MainMenu>
-			<h2>SHOPPP</h2>
-			<ItemBox item={this.state.item} />
-            </div>
-    );
+  constructor (props) {
+    super(props);
+    this.state = {
+      items: []
     }
+  }
+
+  componentDidMount() {
+    Axios.get('/item/all').then((val) => {
+      let items = val.data.map((item) => {
+        return {
+          id: item.id,
+          name: item.nameEn + ' ' + item.size,
+          price: item.price + ' ฿',
+          img_path: item.image
+        }
+      });
+      this.setState({ items: items });
+    });
+  }
+
+  render() {
+    return (
+      <div className="shop-page">
+        <section>
+          <MainMenu>
+            <Link to="/account"><Icon type="user" /> Account </Link>
+            <Link to="/wishlist"><Icon type="star-o" /> Wishlist </Link>
+            <Link to="/checkout"><Icon type="check-circle-o" /> Checkout </Link>
+            <Link to="/cart"><Icon type="shopping-cart" /> Cart </Link>
+            <Link to="/login"><Icon type="login" /> Login </Link>
+          </MainMenu>
+          <div className="banner-img">BANNER</div>
+        </section>
+        <div className="layout">
+          <section>
+            <CategorySideMenu />
+            <OtherSideMenu />
+          </section>
+          <section>
+            <ItemGroup data={this.state.items} title="Items" />
+          </section>
+        </div>
+      </div>
+    );
+  }
 }
