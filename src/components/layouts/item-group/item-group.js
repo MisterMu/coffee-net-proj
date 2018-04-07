91b41122f4a2602a2574f8b1175daa9e90428df8
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-04-07 17:42:07 +0700

    item-group now support store-box

--- src/components/layouts/item-group/item-group.js
+++ src/components/layouts/item-group/item-group.js
@@ -2,19 +2,29 @@ import React from 'react';
 import { Divider, Spin } from 'antd';
 
 import './item-group.scss';
-import { ItemBox } from '..';
+import { ItemBox, StoreBox } from '..';
 
 export class ItemGroup extends React.Component {
   render() {
     let tmp = [];
     if (this.props.data != 0) {
-      tmp = this.props.data.map((item) => {
-        return (
-          <div className="box" >
-            <ItemBox data={item} key={item.id} />
-          </div>
-        );
-      });
+      if (this.props.type === 'stores') {
+        tmp = this.props.data.map((store) => {
+          return (
+            <div className="box">
+              <StoreBox data={store} key={store.id} />
+            </div>
+          );
+        });
+      } else {
+        tmp = this.props.data.map((item) => {
+          return (
+            <div className="box" >
+              <ItemBox data={item} key={item.id} />
+            </div>
+          );
+        });
+      }
     } else {
       tmp = <div className="spin"><Spin /></div>;
     }
