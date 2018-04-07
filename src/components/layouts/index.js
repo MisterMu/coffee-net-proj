6cf149151e103628eb11af5d2ab0d40b9b18bd1f
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-04-07 17:41:06 +0700

    create store-box in component

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -3,3 +3,4 @@ export * from './main-menu/main-menu.js';
 export * from './footer/footer.js';
 export * from './item-group/item-group.js';
 export * from './item-box/item-box.js';
+export * from './store-box/store-box.js';
---
+++ src/components/layouts/store-box/store-box.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+import './store-box.scss';
+
+export class StoreBox extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      nav: false
+    };
+  }
+
+  goToShopPage = () => {
+    this.setState({ nav: true });
+  }
+
+  render() {
+    if (this.state.nav) {
+      return <Redirect to={"/store/" + this.props.data.id} />
+    }
+    else {
+      return (
+        <div className="store-box" onClick={this.goToShopPage}>
+          <div className="img-container"/>
+          <p className="store-name">{this.props.data.name}</p>
+        </div>
+      );
+    }
+  }
+}
