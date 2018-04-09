40ca58d223188b53e8940618ce0fe811d71f3242
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-04-10 00:48:01 +0700

    add shop-list to super-admin flow in admin-router

--- src/routers/admin-router.js
+++ src/routers/admin-router.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Icon } from 'antd';
+import { BrowserRouter, Switch, Route, Redirect, Link } from 'react-router-dom';
+import { MainMenu } from '../components';
+import { ShopListPage } from '../pages';
 
 export class AdminRouter extends React.Component {
   constructor (props) {
@@ -10,13 +13,14 @@ export class AdminRouter extends React.Component {
   }
 
   logout = () => {
+    console.log('logout')
     sessionStorage.removeItem('isLogin');
     localStorage.removeItem('isLogin');
     this.setState({ permission: false });
-    return <Redirect to="/cart" />
+    return null;
   }
 
-  componentWillMount () {
+  chkSession = () => {
     let session = sessionStorage.getItem('isLogin');
     let local = localStorage.getItem('isLogin');
     if (session !== null) {
@@ -28,17 +32,30 @@ export class AdminRouter extends React.Component {
     }
   }
 
+  componentWillMount () {
+    this.chkSession();
+  }
+
   render () {
     if (this.state.permission) {
       return (
-        <BrowserRouter
-          basename="/admin"
-        >
-          <Switch>
-            <Route exact path="/" component={null} />
-            <Route path="/logout" component={this.logout} />
-          </Switch>
-        </BrowserRouter>
+        <div>
+          <BrowserRouter
+            basename="/admin"
+          >
+            <div>
+              <MainMenu>
+                <Link to="/"> Shop list </Link>
+                <Link to="/logout"><Icon type="logout" /> Logout </Link>
+              </MainMenu>
+              <Switch>
+                <Route exact path="/" component={() => <Redirect to="/shop-list" />} />
+                <Route path="/shop-list" component={ ShopListPage } />
+                <Route path="/logout" component={this.logout} />
+              </Switch>
+            </div>
+          </BrowserRouter>
+        </div>
       );
     } else {
       return (
