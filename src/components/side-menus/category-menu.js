f6402b21c7013bc02f536f0b447436dec79e1d15
Pakin Suwannawat <mistermu@Pakins-MacBook-Pro.local>
2018-04-26 20:40:58 +0700

    get category via api

--- src/components/side-menus/category-menu.js
+++ src/components/side-menus/category-menu.js
@@ -3,30 +3,32 @@ import { Menu, Divider } from 'antd';
 import SubMenu from 'antd/lib/menu/SubMenu';
 
 import './styles.scss';
+import Axios from 'axios';
 
 export class CategorySideMenu extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      openKeys: ['CB']
+      items: []
     };
-    this.rootSubmenuKeys = ['CB', 'CM', 'DB'];
-  }
-
-  onOpenChange = (openKeys) => {
-    const lastestOpenKey = openKeys.find(key => this.state.openKeys.indexOf(key) === -1);
-    if (this.rootSubmenuKeys.indexOf(lastestOpenKey) === -1) {
-      this.setState({ openKeys });
-    } else {
-      this.setState({ openKeys: lastestOpenKey? [lastestOpenKey] : []});
-    }
   }
 
   handleClick = (e) => {
     console.log('click', e);
   }
 
-  render() {
+  componentDidMount () {
+    Axios.get('/item/cat').then((res) => {
+      this.setState({ items: res.data });
+    });
+  }
+
+  render () {
+    let items = this.state.items.map((item, i) => {
+      return (
+        <Menu.Item key={i}>{item}</Menu.Item>
+      );
+    });
     return (
       <div className='side-menu'>
         <Divider><span className='title'>CATEGORY</span></Divider>
@@ -36,27 +38,7 @@ export class CategorySideMenu extends React.Component {
           openKeys={this.state.openKeys}
           onOpenChange={this.onOpenChange}
         >
-          <SubMenu key="CB" title={<span>COFFEE BEANS</span>}>
-            <Menu.Item key="1">Special Process Coffee</Menu.Item>
-            <Menu.Item key="2">Peaberry</Menu.Item>
-            <Menu.Item key="3">Delight Coffee</Menu.Item>
-            <Menu.Item key="4">Medium Coffee</Menu.Item>
-            <Menu.Item key="5">Bold</Menu.Item>
-          </SubMenu>
-          <SubMenu key="CM" title={<span>COFFEE MAKERS</span>}>
-            <Menu.Item key="6">Moka Pot</Menu.Item>
-            <Menu.Item key="7">Hand Grinder</Menu.Item>
-            <Menu.Item key="8">French Press</Menu.Item>
-            <Menu.Item key="9">Filter Paper</Menu.Item>
-            <Menu.Item key="10">Dripper & Kettle</Menu.Item>
-          </SubMenu>
-          <SubMenu key="DB" title={<span>DRIP BAGS</span>}>
-            <Menu.Item key="11">Dark Blend</Menu.Item>
-            <Menu.Item key="12">Original Blend</Menu.Item>
-            <Menu.Item key="13">Go Blend</Menu.Item>
-            <Menu.Item key="14">City Roast</Menu.Item>
-            <Menu.Item key="15">Full City Roast</Menu.Item>
-          </SubMenu>
+          { items }
         </Menu>
       </div>
     );
